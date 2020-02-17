148ec051c4a489cb154f426b622eb4ead9ac816f
oliveraladrovic <oliver.aladrovic@gmail.com>
2020-02-17 01:14:57 +0100

    Updates

---
+++ precache-manifest.cbe88d2dfcf9371913d00508f62b3bd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afe4d8f7f7fb93dab06d1281694501a3",
+    "url": "/javascript-calculator/index.html"
+  },
+  {
+    "revision": "8b5fcfc19858123bd9f0",
+    "url": "/javascript-calculator/static/css/main.9f0271de.chunk.css"
+  },
+  {
+    "revision": "617c540be7e84608ac3f",
+    "url": "/javascript-calculator/static/js/2.a216859b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/javascript-calculator/static/js/2.a216859b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b5fcfc19858123bd9f0",
+    "url": "/javascript-calculator/static/js/main.6f929f21.chunk.js"
+  },
+  {
+    "revision": "054eac1b9e1be10a5c81",
+    "url": "/javascript-calculator/static/js/runtime-main.aafb8b0a.js"
+  }
+]);
